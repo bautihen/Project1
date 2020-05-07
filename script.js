0b9aea96946c96fd5b4d8932fd4f6b162eb7b7dd
Jonathan Alpart <panjialang@gmail.com>
2020-05-07 16:16:54 -0500

    commenting in script to help follow the logic

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ var destination;
 // submit button is attached the div with ID: flightform
 $("#GetAirport").on('submit', function (event) {
 	event.preventDefault();
-
+	// get user inputs of cities
 	var departure = $("#Departure").val();
 	var destination = $("#Destination").val();
-	citySearch(departure, "departure");
-	citySearch(destination, "destination");
-	
-	var airportSelectionButton = $("<input>");
-	airportSelectionButton.attr("type", "submit");
+	// run the api call functions for once for each city
+	citySearch(departure, "departure"); // (2nd parameters are for future ID names)
+	citySearch(destination, "destination"); // ^^
+
+	var airportSelectionButton = $("<input>"); // creating new input
+	airportSelectionButton.attr("type", "submit"); // it will be a submit form
 	airportSelectionButton.attr("value", "Get Prices");
 	airportSelectionButton.attr("id", "AirportSelectionButton");
 	var airportContent = $("#AirportCodes");
@@ -20,8 +21,11 @@ $("#GetAirport").on('submit', function (event) {
 })
 
 function citySearch(city, type) {
+	// call up the AirportCode section that we're about to change
 	var airportContent = $("#AirportCodes");
+	// empty out the section for the new incoming stuff
 	airportContent.empty();
+	// 
 	fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=${city}`, {
 		"method": "GET",
 		"headers": {
@@ -50,30 +54,30 @@ function citySearch(city, type) {
 
 
 
- $("#AirportSelectionButton").on('submit', function (event) {
+$("#AirportSelectionButton").on('submit', function (event) {
 	event.preventDefault();
 	console.log("boop");
 	//var departureAirport = $("#departure");
 
-/*
-
-	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-05-07?inboundpartialdate=2020-06-01", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-			"x-rapidapi-key": "ee974abcfbmsh2bbdc194e450a08p14b169jsnf711e272c365"
-		}
-	})
-		.then(response => {
-			return response.json();
+	/*
+	
+		fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-05-07?inboundpartialdate=2020-06-01", {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+				"x-rapidapi-key": "ee974abcfbmsh2bbdc194e450a08p14b169jsnf711e272c365"
+			}
 		})
-		.then(response => {
-			console.log(response);
-		})
-		.catch(err => {
-			console.log(err);
-		}); */
-}) 
+			.then(response => {
+				return response.json();
+			})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			}); */
+})
 
 function currencyExchange() {
 	fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=SGD&to=MYR", {
