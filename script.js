18cbba89c6bef902bc1418dd3986ed4cf041c877
Jonathan Alpart <panjialang@gmail.com>
2020-05-07 12:06:06 -0500

    working on getting prices from new api call

--- script.js
+++ script.js
@@ -19,12 +19,30 @@ function destinationSearch(city) {
 			airRow.attr("id", "airport-picker")
 			airportContent.append(airRow);
 			response.Places.forEach(place => {
-				airRow.append(`<option value="${place.PlaceName}">${place.PlaceName}</option>`);
+				airRow.append(`<option value="${place.PlaceId}">${place.PlaceName}</option>`);
 			})
 		}).catch(err => {
 			console.log(err);
 		});
 }
+
+fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-05-07?inboundpartialdate=2020-06-01", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		"x-rapidapi-key": "ee974abcfbmsh2bbdc194e450a08p14b169jsnf711e272c365"
+	}
+})
+	.then(response => {
+		return response.json();
+	})
+	.then(response => {
+		console.log(response);
+	})
+	.catch(err => {
+		console.log(err);
+	});
+
 $("#flightform").on('submit', function (event) {
 	event.preventDefault();
 	destinationSearch($("#departure").val());
