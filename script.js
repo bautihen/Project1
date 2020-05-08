7ea740d168cd2654740b513c2690a6d978f035ec
Jonathan Alpart <panjialang@gmail.com>
2020-05-08 09:50:32 -0500

    trying to get rid of this post-merge bug of showing the airport dropdowns and button twice

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $("#GetAirports").on('submit', function (event) {
 	var departure = $("#Departure").val();
 	var destination = $("#Destination").val();
 	// run the api call functions for once for each city
-	if (departure !== "" && destination !== "") { 
+	if (departure !== "" && destination !== "") {
 		citySearch(departure, "Departure"); // (2nd parameters are for future ID names)
 		citySearch(destination, "Destination"); // ^^
 		generateFinalButton();
@@ -46,15 +46,16 @@ async function citySearch(city, type) {
 			selectAirports.attr("class", "form");
 			airportsContainer.append(selectAirports);
 
-			
+
 			// creating a select element
 			var chooseAirport = $("<select>")
+			// give the airport selection menu an ID named after city
+			chooseAirport.attr("id", `${type}Airports`)
 			// appending it to the "airports" area of page
 			selectAirports.prepend(chooseAirport);
 			// for each place (airport)
 			response.Places.forEach(place => {
-				// give the airport selection menu an ID named after city
-				chooseAirport.attr("id", `${type}Airports`)
+
 				// value for each selection is the Place ID, name shown is PlaceName
 				chooseAirport.append(`<option value="${place.PlaceId}">${place.PlaceName}</option>`);
 			});
@@ -80,32 +81,32 @@ function generateFinalButton() {
 }
 
 
- function onClick() {
+function onClick() {
 	var departureAirport = $("#DepartureAirports").val();
 	var destinationAirport = $("#DestinationAirports").val();
 	console.log(departureAirport);
 	console.log(destinationAirport);
 };
-	//var departureAirport = $("#departure");
+//var departureAirport = $("#departure");
 
-	/*
+/*
 	
-		fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-05-07?inboundpartialdate=2020-06-01", {
-			"method": "GET",
-			"headers": {
-				"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-				"x-rapidapi-key": "ee974abcfbmsh2bbdc194e450a08p14b169jsnf711e272c365"
-			}
+	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-05-07?inboundpartialdate=2020-06-01", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+			"x-rapidapi-key": "ee974abcfbmsh2bbdc194e450a08p14b169jsnf711e272c365"
+		}
+	})
+		.then(response => {
+			return response.json();
 		})
-			.then(response => {
-				return response.json();
-			})
-			.then(response => {
-				console.log(response);
-			})
-			.catch(err => {
-				console.log(err);
-			}); */
+		.then(response => {
+			console.log(response);
+		})
+		.catch(err => {
+			console.log(err);
+		}); */
 
 
 function currencyExchange() {
