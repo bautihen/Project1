c880c783218b6d905dfe6bd7a72c2db2fa4f98c0
Jonathan Alpart <panjialang@gmail.com>
2020-05-08 12:03:12 -0500

    got both APIs working!

--- script.js
+++ script.js
@@ -1,20 +1,74 @@
 // we'll need two variables, one for each city on each end of the flight
 var departure;
 var destination;
-var cheapFlights = $("#CheapFlights");
-var airports = $("#Airports");
+var cheapFlights = $("#cheap-flights");
+var airports = $("#airport-codes");
 var selectAirports = $("<form>");
 var airportsContainer = $("<figure>");
+var price = 0;
+
+// submit button is attached the div with ID: currency-picker
+$("#currency-picker").on('change', function () {
+	fetch(`https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=USD&to=${this.value}`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+			"x-rapidapi-key": "ee974abcfbmsh2bbdc194e450a08p14b169jsnf711e272c365"
+		}
+	})
+		.then(response => response.json())
+		.then(response => {
+			var conversionRate = response;
+			$("#displayConversion").text(conversionRate * price);
+
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	})
 
 
-// submit button is attached the div with ID: GetAirports
-$("#GetAirports").on('submit', function (event) {
+ // submit button is attached the div with ID: AirportSelectionButton
+ $(".container").on('click', "#AirportSelectionButton", function (event) {
+	event.preventDefault();
+	var departureAirport = $("#DepartureAirports").val();
+	var destinationAirport = $("#DestinationAirports").val();
+	console.log(departureAirport);
+	console.log(destinationAirport);
+
+	fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${departureAirport}/${destinationAirport}/2020-05-12?inboundpartialdate=2020-08-01`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+			"x-rapidapi-key": "ee974abcfbmsh2bbdc194e450a08p14b169jsnf711e272c365"
+		}
+	})
+		.then(response => {
+			return response.json();
+		})
+		.then(response => {
+			console.log(response);
+			price = response.Quotes[0].MinPrice;
+			console.log(price);
+
+			$("#displayUSDPrice").text("$" + price + ".00");
+		})
+		.catch(err => {
+			console.log(err);
+		}); 
+
+
+});
+
+
+// submit button is attached the div with ID: flightform
+$("#flightform").on('submit', function (event) {
 	event.preventDefault();
 	// empty out the section for the new incoming stuff
 	airports.empty();
 	// get user inputs of cities
-	var departure = $("#Departure").val();
-	var destination = $("#Destination").val();
+	var departure = $("#departure").val();
+	var destination = $("#destination").val();
 	// run the api call functions for once for each city
 	if (departure !== "" && destination !== "") {
 		citySearch(departure, "Departure"); // (2nd parameters are for future ID names)
@@ -71,9 +125,8 @@ async function citySearch(city, type) {
 
 function generateFinalButton() {
 	var airportSelectionButton = $("<button>"); // creating new input
-	airportSelectionButton.text("Get Prices");
+	airportSelectionButton.text("Get Price");
 	airportSelectionButton.attr("id", "AirportSelectionButton"); // button ID
-	airportSelectionButton.attr("onClick", onClick());
 
 	selectAirports.append($("<br>"));
 	selectAirports.append($("<br>"));
@@ -81,7 +134,12 @@ function generateFinalButton() {
 }
 
 
+
+
+/*
+
 function onClick() {
+	event.preventDefault();
 	var departureAirport = $("#DepartureAirports").val();
 	var destinationAirport = $("#DestinationAirports").val();
 	console.log(departureAirport);
@@ -89,7 +147,7 @@ function onClick() {
 };
 //var departureAirport = $("#departure");
 
-/*
+
 	
 	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-05-07?inboundpartialdate=2020-06-01", {
 		"method": "GET",
@@ -106,7 +164,7 @@ function onClick() {
 		})
 		.catch(err => {
 			console.log(err);
-		}); */
+		}); 
 
 
 function currencyExchange() {
@@ -123,3 +181,4 @@ function currencyExchange() {
 			console.log(err);
 		});
 }
+*/
